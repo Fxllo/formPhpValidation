cddc1d0eeae6f58c42ea537ae850aa41efab3315
Fxllo <dallagliofilippo32@gmail.com>
2024-02-09 10:40:53 +0100

    update file image new camp

--- formOperation/create.php
+++ formOperation/create.php
@@ -13,6 +13,22 @@ if (isset($_POST['invio'])) {
         $email = $_POST["email"];
     if (!empty($_POST["phoneNumber"]))
         $telefono = $_POST["phoneNumber"];
+    if (isset($_FILES["userFile"])) {
+        $file = $_FILES["userFile"];
+        $fileExt = strtolower(pathinfo($file["name"], PATHINFO_EXTENSION));
+        $allowed = array("jpg", "jpeg", "png");
+        if(!in_array($fileExt, $allowed)){
+            die("Formato non valido");
+        }
+        $maxSize = 5 * 1024 * 1024;
+        if($file["size"] > $maxSize){
+            die("File troppo grande, max 5MB");
+        }
+        $randomString = substr(md5(uniqid()), 0, 10);
+        $newFileName = "tmp_" . $randomString;
+        $uploadDir = "temp/";
+        
+    }
     
     $sql = "INSERT INTO student (name, surname, class, ind, email, phoneNumber) VALUES ('$name', '$surname', '$class', '$indirizzo', '$email', '$telefono')";
     mysqli_query($conn, $sql);
@@ -38,6 +54,14 @@ $html = "
             margin: 10px 0;
             box-sizing: border-box;
         }
+        input[type='file'] {
+            background-color: white;
+            border: 2px solid #ccc;
+            color: #333;
+            font-weight: bold;
+            padding: 5px 10px;
+            cursor: pointer;
+        }
         .back-button {
             position: absolute;
             top: 10px;
@@ -54,7 +78,7 @@ $html = "
 <body>
     <a href='main.php' class='back-button'>Back to Hub</a>
     <div class='form-container'>
-        <form action='create.php' method='post'>
+        <form action='create.php' method='post' enctype='multipart/form-data'>
             <input type='text' name='name' placeholder='Name' required>
             <input type='text' name='surname' placeholder='Surname' required>
             <input type='text' name='class' placeholder='Class' required>
@@ -67,6 +91,7 @@ $html = "
             </select>
             <input type='email' name='email' placeholder='Email' required>
             <input type='tel' name='phoneNumber' placeholder='Phone Number' required>
+            <input type='file' name='userFile' value='Carica Foto'>
             <input type='submit' name='invio' value='Invia'>
         </form>
     </div>
